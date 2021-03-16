70ce651b16ee86008fde1c645ea7d8a2e78c53f9
Chris Wright <chris.wright@nanoporetech.com>
2021-03-16 11:56:14 +0000

    Update __init__.py

--- aplanat/__init__.py
+++ aplanat/__init__.py
@@ -9,7 +9,7 @@ import bokeh.io as bkio
 from bokeh.layouts import gridplot
 from bokeh.plotting import Figure
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 # we don't run a comprehensive test suite and mostly in notebooks,
 # so show warnings all the time.
