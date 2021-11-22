2b358d36c3654c1b9e573cf7b160dd9c04f8eb3a
Sarah Griffiths <sarah.griffiths@nanoporetech.com>
2021-11-22 11:08:16 +0000

    CI-Update

--- aplanat/__init__.py
+++ aplanat/__init__.py
@@ -11,7 +11,7 @@ import bokeh.io as bkio
 from bokeh.layouts import gridplot
 from bokeh.plotting import Figure
 
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 
 # we don't run a comprehensive test suite and mostly in notebooks,
 # so show warnings all the time.
